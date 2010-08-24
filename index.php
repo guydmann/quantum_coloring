738cea43654e3f172e3ce81ab31f498e6ffc2974
Guy Mann <guydmann@gmail.com>
2010-08-24 19:38:42 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<?php include("../../header.php");
+writeheader("Quantum Coloring","javascript, js, coding","A tool for coloring blah blah blah need to say something by Guy Mann");
+?>
+
+<body>
+
+<?php include("../../topbar.phtml"); ?><br>
+<?php include("coloring_app.php"); ?>
+
+</body>
+</html>
+
